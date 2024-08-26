4144edf942fc184ab77f079d2744b8d59b591a5c
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2024-08-26 18:53:34 +0200

    chore: remove useless code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ const appModel = AppModel(localStorage);
 
 appModel.loadStorage();
 
-appModel.addProject("Finishing this ToDo App");
-
-appModel.addToDoToProject(appModel.getIthSubElement(0), "yah", new Date(2040,10,8), "this is a todo", 1, true);
-
-
 const appView = AppView(document, appModel);
 
 const appController = AppController(document, appView, appModel);
